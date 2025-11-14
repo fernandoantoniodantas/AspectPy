f51dcdd0a182402e5526f6ce30cb3520493ff944
Fernando Gomes Pinto <fernando.prefeitura@gmail.com>
2025-11-14 08:08:24 -0300

    Add initial implementation of AspectPy library

---
+++ aspectpy/__init__.py
@@ -0,0 +1,8 @@
+# aspectpy/__init__.py
+# AspectPy - A simple aspect-oriented programming library for Python
+# Developed by Fernando Pinto <fpinto@inf-puc.rio.br>
+# version 0.1.0
+from .pointcut import pointcut
+from .aspect import aspect
+from .joinpoint import JoinPoint
+from .weaver import weave_all
